c3debc95bc8938a7c8f68c3b610d589c6250447a
Christian <github@agentperry.de>
2013-08-18 23:24:47 +0200

    GPG encryption, change master password, bugfixes, nano integration

--- passwordmanager.sh
+++ passwordmanager.sh
@@ -1,32 +1,20 @@
 #!/bin/bash
 
-# Get User Info
-USER=$(id -u -n)
-GROUP=$(id -g -n)
 
-# Set PWROOT
-PWROOT="$(cat /etc/passwd | grep -E "^$USER:" | cut -d ':' -f 6)/passwordmanager"
-
-# Create passwordmanager directory
-if [ ! -d "$PWROOT" ]; then
-   mkdir -p "$PWROOT"
-fi
-
-cd "$PWROOT"
-
-# Initialize GIT repo
-if [ ! -d "$PWROOT/.git" ]; then
-   git init
-   echo "password.txt" > .gitignore
-   git add -A
-   git commit -m "Created git repo and added .gitignore"
-fi
-
-# Set permissions
-chown -R $USER:$USER "$PWROOT"
-chmod u=rwx,go=- "$PWROOT"
-
-
+function readpassword() {
+   unset password
+   prompt="$1: "
+   while IFS= read -p "$prompt" -r -s -n 1 char
+   do
+      if [[ $char == $'\0' ]]
+      then
+          break
+      fi
+      prompt='*'
+      password+="$char"
+   done
+   echo $password
+}
 
 function waitforenter() {
    echo
@@ -36,18 +24,39 @@ function waitforenter() {
 
 function showpasswords() {
    clear
-   cat "$PWROOT/passwords.txt"
+   echo
+   echo "Zeige Passwörter an:"
+   echo "----------------------"
+   echo
+
+   if [ -f "$PWFILE.gpg" ]; then
+      echo "$PW" | gpg -q --batch --yes --output - --passphrase-fd 0 "$PWFILE.gpg" 2> /dev/null
+   else
+      echo "Keine verschlüsselte Passwortdatei gefunden"
+   fi
+
+   echo
+   echo "----------------------"
    waitforenter
 }
 
 function editpasswords() {
-   echo "vim ist ein Editor der nur mit der Tastatur bedient wird."
-   echo "Zum einfügen von Text muss in den Einfügen-Modus mit der Taste 'i' gewechselt werden."
-   echo "Zum Ende mit ESC aus dem Einfügen-Modus aussteigen."
-   echo "Mit :x speichern und beenden, mit :q! ohne Speichern beenden."
+   echo "$1 ist ein Editor der nur mit der Tastatur bedient wird."
+   if [ "$1" == "vim" ]; then
+      echo "Zum einfügen von Text muss in den Einfügen-Modus mit der Taste 'i' gewechselt werden."
+      echo "Zum Ende mit ESC aus dem Einfügen-Modus aussteigen."
+      echo "Mit :x speichern und beenden, mit :q! ohne Speichern beenden."
+   else
+      echo "Unterhalb des Editors sind alle Tastenkombinationen aufgelistet."
+      echo "Das ^ Zeichen entspricht dabei der Strg-Taste."
+   fi
    waitforenter
 
-   vim "$PWROOT/passwords.txt"
+   echo "$PW" | gpg -q --batch --yes --passphrase-fd 0 "$PWFILE.gpg"
+   $1 "$PWFILE"
+   echo "$PW" | gpg -q --batch --yes --passphrase-fd 0 -c "$PWFILE"
+   rm "$PWFILE"
+
    clear
    echo "Änderungen in Versionsverwaltung sichern?"
    echo -n "ja oder nein?> "
@@ -61,6 +70,13 @@ function editpasswords() {
    fi
 }
 
+function unknownoption() {
+   clear
+   echo
+   echo "Ungültige Option"
+   waitforenter
+}
+
 function changepassword() {
    clear
    echo "Das alte Passwort eingeben, danach zwei mal das neue."
@@ -70,6 +86,112 @@ function changepassword() {
    waitforenter
 }
 
+function changemasterpassword() {
+   local TMPPWA=$(readpassword "Neues Masterkennwort eingeben")
+   echo
+   local TMPPWB=$(readpassword "Neues Masterkennwort wiederholen")
+   echo
+
+   if [ "$TMPPWA" == "$TMPPWB" ]; then
+      echo "$PW" | gpg -q --batch --yes --passphrase-fd 0 "$PWFILE.gpg" 2> /dev/null
+      PW="$TMPPWA"
+      echo "$PW" | gpg -q --batch --yes --passphrase-fd 0 -c "$PWFILE" 2> /dev/null
+      rm "$PWFILE"
+      echo
+      git add -A
+      git commit -m "Masterkennwort geändert! $(date)"
+      echo
+      echo "Masterkennwort erfolgreich geändert!"
+      waitforenter
+   else
+      echo "Kennwörter stimmen nicht überein!"
+      waitforenter
+   fi
+}
+
+function checkmasterpw() {
+   if [ -f "$PWFILE.gpg" ]; then
+      echo "$1" | gpg -q --batch --yes --output - --passphrase-fd 0 "$PWFILE.gpg" 2> /dev/null > /dev/null
+      echo $?
+   else
+      echo 1
+   fi
+}
+
+function checkprogram() {
+   local exist=0
+   type $1 >/dev/null 2>&1 || { local exist=1; }
+   echo "$exist"
+}
+
+
+
+echo
+echo "Prüfe auf alle benötigten Programme..."
+ERROR=0
+for PROG in gpg vim cat chown chmod git nano read;
+do
+   echo -n "Programm $PROG "
+   if [ ! "$(checkprogram "$PROG")" == "0" ]; then
+      ERROR=1
+      echo "nicht gefunden!"
+   else
+      echo "gefunden!"
+   fi
+done;
+echo
+
+if [ "$ERROR" == "1" ]; then
+   echo "Einige Programme wurden nicht gefunden! Bitte nachinstallieren!"
+   waitforenter
+   exit 1
+fi
+
+
+
+# Get User Info
+USER=$(id -u -n)
+GROUP=$(id -g -n)
+
+# Set PWROOT
+PWROOT="$(cat /etc/passwd | grep -E "^$USER:" | cut -d ':' -f 6)/passwordmanager"
+PWFILE="$PWROOT/passwords.txt"
+
+# Create passwordmanager directory
+if [ ! -d "$PWROOT" ]; then
+   mkdir -p "$PWROOT"
+fi
+
+cd "$PWROOT"
+
+# Initialize GIT repo
+if [ ! -d "$PWROOT/.git" ]; then
+   git init
+   echo "$PWFILE" > .gitignore
+   git add -A
+   git commit -m "Created git repo and added .gitignore"
+fi
+
+# Set permissions
+chown -R $USER:$USER "$PWROOT"
+chmod u=rwx,go=- "$PWROOT"
+
+# Enter masterpw
+PW=$(readpassword "Bitte Masterkennwort eingeben")
+RESULT=$(checkmasterpw "$PW")
+
+echo $RESULT
+
+# Check MasterPW
+if [ "$RESULT" == "2" ]; then
+   echo "Masterpasswort ist nicht korrekt!"
+   echo "Verbindung wird beendet!"
+   waitforenter
+   exit 1
+fi
+
+
+
 # Run endless while
 while true; do
 
@@ -78,7 +200,10 @@ echo "Was möchtest Du machen?"
 echo
 echo "1] Passwörter anzeigen"
 echo "2] Passwörter mit vim bearbeiten"
-echo "3] Login Passwort ändern"
+echo "3] Passwörter mit nano bearbeiten"
+echo "4] Login Passwort ändern"
+echo "5] Masterkennwort ändern"
+echo
 echo "9] Ausloggen"
 echo
 
@@ -92,15 +217,27 @@ case $INPUT in
    ;;
 
 2)
-   editpasswords
+   editpasswords vim
    ;;
 
 3)
+   editpasswords nano
+   ;;
+
+4)
    changepassword
+   ;;
+
+5)
+   changemasterpassword
    ;;
 
 9)
    break;
+   ;;
+
+*)
+   unknownoption
    ;;
 
 esac
